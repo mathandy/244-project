855fc1c4496132ad4847b8f3592bff1dd6859fd6
Andy Port <andyaport@gmail.com>
2020-03-01 16:30:56 -0800

    add loader stub

--- loader.py
+++ loader.py
@@ -10,3 +10,13 @@ def get_file_info(filename):
             fn1, junk, label = line.strip().split(',')
             file_info.append((fn1.strip(), junk.strip(), label.strip()))
     return file_info
+
+
+def loader():
+    """Return a 6 (unshuffled) tf dataset objects:
+        clean_audio, clean_audio_filepath, noisy_audio, noisy_audio_filepath, noise type, label
+
+        or whatever you think is the best way...
+    """
+    # `tf.audio.decode_wav` might be helpful
+    # we want the audio stored in a way similar to  `scipy.io.wavfile.read(fn)`
