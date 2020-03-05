9ac839681e1d489aee2cc47f7b6ebb0c6bf123f6
Zhengkai Jiang <justinjiang1201@gmail.com>
2020-03-05 08:22:38 -0800

    cnn work in progress, bad alloc error

--- main.py
+++ main.py
@@ -78,6 +78,20 @@ def predict(self, batch_audio: List[np.ndarray], **kwargs) -> List[str]:
     return predictions
 
 
+def loss(model, x, y, training):
+    # training=training is needed only if there are layers with different
+    # behavior during training versus inference (e.g. Dropout).
+    y_ = model(x, training=training)
+    loss_object = tf.keras.losses.MSE
+    return loss_object(y_true=y, y_pred=y_)
+
+
+def grad(model, inputs, targets):
+    with tf.GradientTape() as tape:
+        loss_value = loss(model, inputs, targets, training=True)
+    return loss_value, tape.gradient(loss_value, model.trainable_variables)
+
+
 if __name__ == '__main__':
     # test()
 
@@ -95,14 +109,13 @@ if __name__ == '__main__':
     # clean_wavs_padded = np.array([pad(x) for x in clean_wavs]).astype('float32')
 
     ### Seems do not need to pad the wav files
-    clean_wavs_padded = clean_wavs
     # normalize
-    max_ = 0
-    for i in range(len(clean_wavs_padded)):
-        temp_max = clean_wavs_padded[i].max()
-        if max_ < temp_max:
-            max_ = temp_max
-    clean_wavs_padded = [ x / max_ for x in clean_wavs_padded]
+    # max_ = 0
+    # for i in range(len(clean_wavs)):
+    #     temp_max = clean_wavs[i].max()
+    #     if max_ < temp_max:
+    #         max_ = temp_max
+    # clean_wavs = [x / max_ for x in clean_wavs]
     # clean_wavs_padded = clean_wavs_padded / clean_wavs_padded.max()
 
     pretrained_pipeline = asr.load('deepspeech2', lang='en')
@@ -113,24 +126,49 @@ if __name__ == '__main__':
     encoded_transcripts_padded = \
         np.array([pad(x, 91) for x in encoded_transcripts])
 
-    features = pretrained_pipeline._features_extractor(clean_wavs_padded)
+    features_clean = pretrained_pipeline._features_extractor(clean_wavs)
+    features_noisy = pretrained_pipeline._features_extractor(noisy_wavs)
 
-    train_data = (np.expand_dims(features, -1), encoded_transcripts_padded)
-    val_data = train_data
+    train_data_clean = (np.expand_dims(features_clean, -1), encoded_transcripts_padded)
+    train_data_noisy = (np.expand_dims(features_noisy, -1), encoded_transcripts_padded)
+
+    features_clean = np.expand_dims(features_clean, -1)
+    features_noisy = np.expand_dims(features_noisy, -1)
 
     # working full pipeline inference example
     # we could use https://www.tensorflow.org/tutorials/customization/autodiff
-    batch_size = 10
-    # den = simple_denoiser(features[:batch_size, :, :, None])
-    feature_batch = np.expand_dims(features[:batch_size], -1)
-    den = simple_denoiser(feature_batch)
-    y = pretrained_pipeline._model(den)
+    batch_size = 2
 
-    predictions = pretrained_pipeline.decoder(y)
-    decoded_predictions = \
-        [[pretrained_pipeline._alphabet._label_to_str[char] for char in l]
-         for l in predictions]
-    print(decoded_predictions)
+    model = tf.keras.Sequential([
+        tfkl.Conv2D(64, 3, name='den_conv0', padding='SAME'),
+        tfkl.Conv2D(64, 3, name='den_conv1', padding='SAME'),
+        tfkl.Conv2D(1, 3, name='den_conv2', padding='SAME')
+    ])
+    # den = simple_denoiser(features_noisy[:batch_size, :, :, None])
+
+    optimizer = tf.keras.optimizers.SGD(learning_rate=0.01)
+    loss_value, grads = grad(model, features_noisy[:batch_size], features_clean[:batch_size])
+
+    print("Step: {}, Initial Loss: {}".format(optimizer.iterations.numpy(), loss_value.numpy()))
+    optimizer.apply_gradients(zip(grads, model.trainable_variables))
+    print("Step: {},          Loss: {}".format(optimizer.iterations.numpy(),
+                                               loss(model, features_noisy, features_clean, training=True).numpy()))
+
+    # feature_batch = np.expand_dims(features[:batch_size], -1)
+    # ## previous denoiser
+    # # den = simple_denoiser(feature_batch)
+    # y = pretrained_pipeline._model(den)
+    #
+    # predictions = pretrained_pipeline.decoder(y)
+    # decoded_predictions = \
+    #     [[pretrained_pipeline._alphabet._label_to_str[char] for char in l]
+    #      for l in predictions]
+    # print(decoded_predictions)
+
+
+
+
+
 
     # FIX THIS CODE BELOW
     # m = our_model(pretrained_pipeline, train_data[0].shape[1:])
