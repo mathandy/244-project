3de423b8a3fc36aa77cc195945b9ea1ac920d685
Zhengkai Jiang <justinjiang1201@gmail.com>
2020-03-07 19:54:17 -0800

    added the python file to create noise

---
+++ create_noise.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from scipy.io import wavfile
+source_file = pd.read_csv("data/train.csv")
+fs, noise = wavfile.read("noise/AirConditioner_1.wav")
+noise = noise.astype('float32')
+noisy_file_paths = []
+ratio = 10
+for i in range(len(source_file["clean_file"])):
+    clean_src = source_file.iloc[i, 0]
+    noisy_dst = source_file.iloc[i, 2]
+    fs, clean = wavfile.read(clean_src)
+    clean = clean.astype('float32')
+    noisy_file = (clean * (ratio - 1) // ratio + noise[:len(clean)] // ratio) / 32768
+    wavfile.write(noisy_dst, fs, noisy_file)
